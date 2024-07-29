68f04b553e9ff1f169c37422fe2204b03a606efb
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-07-29 21:04:01 +0300

    adding scroll view in sign SignupPage.dart and LoginPage.dart

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -18,53 +18,55 @@ class _LoginpageState extends State<Loginpage> {
         appBar: AppBar(
           title: Text("ANON BETTING APP",style: TextStyle(color: Colors.blue[1000],fontWeight: FontWeight.bold),),centerTitle: true,backgroundColor: Colors.blue[300],), backgroundColor: Colors.grey[200],
 
-        body: Center(
-          child: Column(
-            children: [
-              Text("WELCOME TO ANON",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue[900]),),
-
-              Text("LOGIN", style: GoogleFonts.pacifico(fontSize: 40,fontWeight: FontWeight.bold, color:  Colors.blue[900])),
-
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 70, 50, 10),
-                child: TextField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    hintText: ("012345678"),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-                      prefixIcon: Icon(Icons.phone_android)
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
-                child: TextField(
-                  keyboardType: TextInputType.visiblePassword,
-                  decoration: InputDecoration(
-                    hintText: ("password"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Text("WELCOME TO ANON",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue[900]),),
+          
+                Text("LOGIN", style: GoogleFonts.pacifico(fontSize: 40,fontWeight: FontWeight.bold, color:  Colors.blue[900])),
+          
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 70, 50, 10),
+                  child: TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      hintText: ("012345678"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        prefixIcon: Icon(Icons.phone_android)
                     ),
-                    prefixIcon: Icon(Icons.password),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(onPressed: (){}, child: Text("LOGIN")),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                child: Row(children: [
-                  Text("If you dont have an account click here"),
-                  IconButton(onPressed: (){
-                    Navigator.push(context,MaterialPageRoute(builder: (context)=>Signuppage()));
-                  }, icon: Icon(Icons.account_circle_outlined), )
-                ],),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+                  child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      hintText: ("password"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      prefixIcon: Icon(Icons.password),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ElevatedButton(onPressed: (){}, child: Text("LOGIN")),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                  child: Row(children: [
+                    Text("If you dont have an account click here"),
+                    IconButton(onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>Signuppage()));
+                    }, icon: Icon(Icons.account_circle_outlined), )
+                  ],),
+                )
+              ],
+            ),
           ),
         ),
 
--- lib/SignupPage.dart
+++ lib/SignupPage.dart
@@ -17,78 +17,80 @@ class _SignuppageState extends State<Signuppage> {
       home: Scaffold(
         appBar: AppBar(
           title: Text("ANON BETTING APP",style: TextStyle(color: Colors.blue[1000],fontWeight: FontWeight.bold),),centerTitle: true,backgroundColor: Colors.blue[300],), backgroundColor: Colors.grey[200],
-        body: Center(
-          child: Column(
-            children: [
-              Text("WELCOME TO ANON",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue[900]),),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Text("WELCOME TO ANON",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue[900]),),
 
-              Text("SIGNUP", style: GoogleFonts.pacifico(fontSize: 40,fontWeight: FontWeight.bold, color:  Colors.blue[900])),
+                Text("SIGNUP", style: GoogleFonts.pacifico(fontSize: 40,fontWeight: FontWeight.bold, color:  Colors.blue[900])),
 
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 20, 50, 10),
-                child: TextField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                      hintText: ("012345678"),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 20, 50, 10),
+                  child: TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        hintText: ("012345678"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        prefixIcon: Icon(Icons.phone_android)
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(50,20,50,0),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: ("email"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      prefixIcon: Icon(Icons.email)
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+                  child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      hintText: ("password"),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10)
                       ),
-                      prefixIcon: Icon(Icons.phone_android)
-                  ),
-                ),
-              ),
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(50,20,50,0),
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: ("email"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)
+                      prefixIcon: Icon(Icons.password),
                     ),
-                    prefixIcon: Icon(Icons.email)
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
-                child: TextField(
-                  keyboardType: TextInputType.visiblePassword,
-                  decoration: InputDecoration(
-                    hintText: ("password"),
-                    border: OutlineInputBorder(
+
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+                  child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      hintText: ("confirm password"),
+                      border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10)
+                      ),
+                      prefixIcon: Icon(Icons.password)
                     ),
-                    prefixIcon: Icon(Icons.password),
                   ),
                 ),
-              ),
-              
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
-                child: TextField(
-                  keyboardType: TextInputType.visiblePassword,
-                  decoration: InputDecoration(
-                    hintText: ("confirm password"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    prefixIcon: Icon(Icons.password)
-                  ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ElevatedButton(onPressed: (){}, child: Text("SIGNUP")),
                 ),
-              ),
 
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(onPressed: (){}, child: Text("SIGNUP")),
-              ),
-
-              Text("If you have an account click here"),
-              IconButton(onPressed: (){
-                Navigator.push(context,MaterialPageRoute(builder: (context)=>Loginpage()));
-              }, icon: Icon(Icons.account_circle_outlined), ),
-              Text("Terms and condition ")
-            ],
+                Text("If you have an account click here"),
+                IconButton(onPressed: (){
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=>Loginpage()));
+                }, icon: Icon(Icons.account_circle_outlined), ),
+                Text("Terms and condition ")
+              ],
+            ),
           ),
         ),
       ),
