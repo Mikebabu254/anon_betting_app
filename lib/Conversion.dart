012ac3f1c782e74c688775aef8bc9a9c26736b9d
Eshi <michaelndolo17142@gmail.com>
2024-08-27 12:58:36 +0300

    adding functionality in Conversion.dart

--- lib/Conversion.dart
+++ lib/Conversion.dart
@@ -6,9 +6,50 @@ class ksh_anon_Conversion extends StatefulWidget {
   @override
   State<ksh_anon_Conversion> createState() => _ksh_anon_ConversionState();
 }
+  class _ksh_anon_ConversionState extends State<ksh_anon_Conversion> {
 
-class _ksh_anon_ConversionState extends State<ksh_anon_Conversion> {
   final TextEditingController ksh = TextEditingController();
+  double cash = 10;
+  double chip = 100.0;
+
+  void convertKsh_chip(){
+    setState(() {
+      double kshValue = double.tryParse(ksh.text)??0;
+      if(kshValue>cash){
+        showDialog(context: context, builder: (BuildContext context){
+          return AlertDialog(
+            title: Text("Insufficient Fund"),
+            content: Text("You dont have enough cash to convert"),
+            actions: [
+              TextButton(
+                child: Text("OK"),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              ),],
+          );
+        },);
+      }else if(kshValue<=0){
+        showDialog(context: context, builder: (BuildContext context){
+          return AlertDialog(
+            title: Text("Error converting"),
+            content: Text("You cant convert less than ksh: 0"),
+            actions: [
+              TextButton(onPressed: (){
+                Navigator.of(context).pop();
+              }, child: Text("OK"))
+            ],
+          );
+        });
+      } else{
+        double conv = kshValue *10;
+        cash = cash-kshValue ;
+        chip = chip +conv;
+      }
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +59,8 @@ class _ksh_anon_ConversionState extends State<ksh_anon_Conversion> {
         body: Center(
           child: Column(
             children: [
+              Text("ksh $cash", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+              Text("Chip : $chip", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
               Padding(
                 padding: const EdgeInsets.all(20),
                 child: TextField(
@@ -31,7 +74,8 @@ class _ksh_anon_ConversionState extends State<ksh_anon_Conversion> {
 
                   ),
                 ),
-              )
+              ),
+              ElevatedButton(onPressed: convertKsh_chip, child: Text("convert"))
             ],
           ),
         ),
