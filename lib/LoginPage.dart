6f6f0317b9d28f8d0d530899ccd3476c826875e9
Eshi <michaelndolo17142@gmail.com>
2024-08-28 13:20:34 +0300

    encounter a problem with database

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -12,33 +12,45 @@ class Loginpage extends StatefulWidget {
 }
 
 class _LoginpageState extends State<Loginpage> {
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
 
-  Future<void> _login() async{
+  Future<void> _login() async {
+    setState(() {
+      isLoading = true; // Show loader
+    });
+
     final email = emailController.text;
     final password = passwordController.text;
-    
-    try{
+
+    try {
       final response = await Supabase.instance.client.auth.signInWithPassword(
-          email: email,
-          password: password,);
-      if(response.user !=null){
-        Navigator.push(context,MaterialPageRoute(builder: (context) => Userdashboard()));
-      }else{
+        email: email,
+        password: password,
+      );
+      if (response.user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Userdashboard()),
+        );
+      } else {
         showErrorSnackBar('Login failed');
       }
-    }catch(e){
+    } catch (e) {
       showErrorSnackBar(e.toString());
+    } finally {
+      setState(() {
+        isLoading = false; // Hide loader
+      });
     }
   }
-  showErrorSnackBar(String message){
+
+  void showErrorSnackBar(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)),
+      SnackBar(content: Text(message)),
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,26 +58,47 @@ class _LoginpageState extends State<Loginpage> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("ANON BETTING APP",style: TextStyle(color: Colors.blue[1000],fontWeight: FontWeight.bold),),centerTitle: true,backgroundColor: Colors.blue[300],), backgroundColor: Colors.grey[200],
-
+          title: Text(
+            "ANON BETTING APP",
+            style: TextStyle(
+              color: Colors.blue[1000],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue[300],
+        ),
+        backgroundColor: Colors.grey[200],
         body: SingleChildScrollView(
           child: Center(
             child: Column(
               children: [
-                Text("WELCOME TO ANON",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue[900]),),
-          
-                Text("LOGIN", style: GoogleFonts.pacifico(fontSize: 40,fontWeight: FontWeight.bold, color:  Colors.blue[900])),
-          
+                Text(
+                  "WELCOME TO ANON",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[900],
+                  ),
+                ),
+                Text(
+                  "LOGIN",
+                  style: GoogleFonts.pacifico(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[900],
+                  ),
+                ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(50, 70, 50, 10),
                   child: TextField(
-                    keyboardType: TextInputType.phone,
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
                     decoration: InputDecoration(
-                      hintText: ("012345678"),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)
-                        ),
-                        prefixIcon: Icon(Icons.phone_android)
+                      hintText: "me@mail.com",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: Icon(Icons.email),
                     ),
                   ),
                 ),
@@ -73,35 +106,50 @@ class _LoginpageState extends State<Loginpage> {
                   padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
                   child: TextField(
                     keyboardType: TextInputType.visiblePassword,
+                    controller: passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
-                      hintText: ("password"),
+                      hintText: "password",
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      prefixIcon: Icon(Icons.password),
+                      prefixIcon: Icon(Icons.lock),
                     ),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(10),
                   child: ElevatedButton(
-                      onPressed: _login, child: Text("LOGIN")),
+                    onPressed: isLoading ? null : _login, // Disable button when loading
+                    child: isLoading
+                        ? CircularProgressIndicator(
+                      color: Colors.red,
+                    )
+                        : Text("LOGIN"),
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                  child: Row(children: [
-                    Text("If you dont have an account click here"),
-                    IconButton(onPressed: (){
-                      Navigator.push(context,MaterialPageRoute(builder: (context)=>Signuppage()));
-                    }, icon: Icon(Icons.account_circle_outlined), )
-                  ],),
-                )
+                  child: Row(
+                    children: [
+                      Text("If you don't have an account click here"),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Signuppage()),
+                          );
+                        },
+                        icon: Icon(Icons.account_circle_outlined),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
         ),
-
       ),
     );
   }
